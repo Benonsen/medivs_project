a8b20cabadd262ba8c73c7e5c32c232631601a82
Benno Steinegger <e12117772@student.tuwien.ac.at>
2025-01-01 22:43:12 +0100

    first progress with displaying correct lines

--- csoList.py
+++ csoList.py
@@ -2,13 +2,7 @@ import numpy as np
 import vtk
 
 def initCSOList():
-    setupCSOList()
     registerForNotification()
-
-
-def setupCSOList():
-    csoList = _getCSOList()
-
 
 def registerForNotification():
     csoList = _getCSOList()
@@ -19,31 +13,25 @@ def csoFinished(_arg):
     if len(csoList.getCSOs()) == 2:
       directionVectors = []
       for cso in csoList.getCSOs():
-          # see https://slicer.readthedocs.io/en/latest/developer_guide/script_repository.html#measure-angle-between-two-markup-lines
-          # returns all important points of the line, just use the first and the last one as start/ending points and create using these two points a vector for the line
           path_points = cso.getPathPoints()
           start = np.array(path_points[0:3])
           end = np.array(path_points[-3:])
-          # todo: only use x and y coordinates and ignore z
-          #start = start[0:1]
-          #end = end[0:1]
-
           directionVector = (end - start) / np.linalg.norm(end - start)
-          directionVectors.append(directionVector)     
-
+          directionVectors.append(directionVector)  
       
       radians = vtk.vtkMath.AngleBetweenVectors(directionVectors[0], directionVectors[1])
       cobb_angle = vtk.vtkMath.DegreesFromRadians(radians)
       ctx.field("baseClassification").value = classify(cobb_angle)
       ctx.field("baseAngle0").value = "Cobb-angle: " + str(round(cobb_angle, 2)) + "°"
+      str_arr1 = " ".join(map(str, directionVectors[0] + start)) 
+      str_arr2 = " ".join(map(str, directionVectors[0] * 1000))  
+      ctx.field("baseFirstVector").value = f"[{str_arr1}, {str_arr2}]"
+      ctx.field("baseSecondVector").value = "[ " + str(directionVectors[1] * 5) + " ]"
 
     if len(csoList.getCSOs()) > 2:
         csoList.removeAll()
           
 def classify(angle):
-    # according to: A Deep Learning Approach for Automatic Scoliosis Cobb Angle Identification
-    # https://ieeexplore.ieee.org/document/9817290
-    # ACCEPTED CLINICAL COBB ANGLE SCOLIOSIS CATEGORY
     if angle <= 10: 
         return "Normal spine"
     if angle <= 20:
@@ -56,5 +44,3 @@ def classify(angle):
 
 def _getCSOList():
     return ctx.field("CSOListContainer.outCSOList").object()
-  
-
