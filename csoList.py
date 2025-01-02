de710206adf3a2e93dee2d78397711745ad8cee8
Benno Steinegger <e12117772@student.tuwien.ac.at>
2025-01-02 12:05:33 +0100

    added extrapolated lines

--- csoList.py
+++ csoList.py
@@ -12,21 +12,27 @@ def csoFinished(_arg):
     csoList = _getCSOList()
     if len(csoList.getCSOs()) == 2:
       directionVectors = []
+      startVectors = []
+      endVectors = []
       for cso in csoList.getCSOs():
           path_points = cso.getPathPoints()
           start = np.array(path_points[0:3])
+          startVectors.append(start)
           end = np.array(path_points[-3:])
-          directionVector = (end - start) / np.linalg.norm(end - start)
+          endVectors.append(end)
+          directionVector = (end - start)
           directionVectors.append(directionVector)  
       
       radians = vtk.vtkMath.AngleBetweenVectors(directionVectors[0], directionVectors[1])
       cobb_angle = vtk.vtkMath.DegreesFromRadians(radians)
       ctx.field("baseClassification").value = classify(cobb_angle)
       ctx.field("baseAngle0").value = "Cobb-angle: " + str(round(cobb_angle, 2)) + "°"
-      str_arr1 = " ".join(map(str, directionVectors[1] + start)) 
-      str_arr2 = " ".join(map(str, (directionVectors[1] + start) * 10))  
+      str_arr1 = " ".join(map(str, (directionVectors[0] * -10) + startVectors[0])) 
+      str_arr2 = " ".join(map(str, (directionVectors[0] * 10) + startVectors[0]))  
       ctx.field("baseFirstVector").value = f"[{str_arr1}, {str_arr2}]"
-      ctx.field("baseSecondVector").value = "[ " + str(directionVectors[1] * 5) + " ]"
+      str_arr1 = " ".join(map(str, (directionVectors[1] * -10) + startVectors[1])) 
+      str_arr2 = " ".join(map(str, (directionVectors[1] * 10) + startVectors[1]))  
+      ctx.field("baseSecondVector").value = f"[{str_arr1}, {str_arr2}]"
 
     if len(csoList.getCSOs()) > 2:
         csoList.removeAll()
